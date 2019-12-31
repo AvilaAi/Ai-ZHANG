3ee9f9ed552beb4a8f24dd8aac15c8b4086f50e2
AvilaAi <avila911008@gmail.com>
2019-12-31 15:31:25 +0100

    aboutme update

--- src/Component/Aboutme.js
+++ src/Component/Aboutme.js
@@ -6,11 +6,10 @@ const mots = [
 	'Polyvalente',
 	'Ouverte d’esprit',
 	'Créative',
-	'Dynamique',
 	'Curieuse',
 	'Motivée',
 	'Efficace',
-	'Sens de l’écoute',
+	'Sens de la logique',
 	"Sens de l'humour",
 ];
 const Aboutme = props => {
@@ -18,37 +17,42 @@ const Aboutme = props => {
 		<Container>
 			<div id='aboutme'>
 				<div className='card'>
-					<div className='title'>
-						{' '}
-						Ai・ZHANG <span>Développeuse web junior・Fullstack</span>
-					</div>
-
-					{mots.map((x, i) => {
-						return <Chip className='chip' label={x} key={i} />;
-					})}
-					<div className='content'>
-						<div className='desc'>
-							{/* DOMAINES DE COMPÉTENCES Maîtrise des différentes techniques de vente (plus de 5 ans
-							d’expériences en vente) et de la gestion de conflits. Aptitude à gérer une boutique
-							(merchandising, stocks, achats, suivi clientèle, suivi du CA, des objectifs qualitatifs et
-							quantitatifs). Aisance dans les relations publiques (facilité à développer et entretenir des
-							liens de longue durée avec les différents interlocuteurs). Capacité à organiser des
-							événements et des voyages. Connaissances linguistiques (anglais, français, chinois mandarin
-							et chinois teochew). Je suis un développeur back et front-end. Curieux et passionné à propos
-							de tout ce qui touche au web, j'essaie de rester à jour sur les technologies du web. J'aime
-							travailler sur des projets variés: développement de sites internet avec PHP7 et MySQL,
-							intégrer des sites internet avec HTML5 / CSS3 et jQuery, développer des sites administrables
-							avec des CMS comme Wordpress. Une veille quotidienne et une polyvalence issue d'un parcours
-							professionnel riche en expériences sont parmi les atouts que je souhaite à présent partager
-							au sein d'une nouvelle équipe. */}
-							123
+					<div className='maincard'>
+						<div className='title'>
+							{' '}
+							<span>Ai・ZHANG</span>Développeur web junior・Fullstack
 						</div>
-						<img
-							width='200px'
-							height='200px'
-							src='https://res.cloudinary.com/dg0flooxn/image/upload/e_auto_color,r_0/v1577528472/lobpaaxetpmb7rn8z3ta.jpg'
-							alt='ai-zhang'
-						/>
+						<hr />
+						{mots.map((x, i) => {
+							return <Chip className='chip' label={x} key={i} />;
+						})}
+						<div className='content'>
+							<div className='desc'>
+								<p>
+									« Curieuse et passionnée à propos de tout ce qui touche au web, je me suis
+									réorientée depuis les métiers du marketing. Ayant un bon sens de la logique, j'ai
+									vite progressé pendant un an d'experience dans le monde de code. En plus une aisance
+									dans les relations humaines, j'apprécis notament le travail en équipe.
+								</p>
+								<p>
+									En tant que développeur polyvalent, je maîtrise les technologies pour le fontend et
+									le backend, j'ai pu réalisé plusieur projets web personnels qui m'a permis
+									d'approfondir mes connaissances surtout dans la language JavaScript, j'aime aussi
+									découvrir de nouvelles techenologies.
+								</p>
+
+								<p>
+									Je suis actuellement à la recherche d'un emploi à plein temps en tant que
+									développeur web et j'aurai plaisir d'intégrer une nouvelle équipe. »
+								</p>
+							</div>
+							<img
+								width='200px'
+								height='200px'
+								src='https://res.cloudinary.com/dg0flooxn/image/upload/e_auto_color,r_0/v1577528472/lobpaaxetpmb7rn8z3ta.jpg'
+								alt='ai-zhang'
+							/>
+						</div>
 					</div>
 				</div>
 			</div>

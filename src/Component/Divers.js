3b9b3c71b7b774f3ea238ef593883d46e23ca0ec
AvilaAi <avila911008@gmail.com>
2020-01-02 00:07:47 +0100

    page divers update

--- src/Component/Divers.js
+++ src/Component/Divers.js
@@ -1,25 +1,46 @@
 import React from 'react';
 import Container from './Container';
-
+import word from '../logo/WordArt.png';
 const Divers = props => {
 	return (
 		<Container>
 			<div className='divers'>
 				<div className='card one'></div>
 				<div className='card two'>
-					Co-fondatrice de l'Association LifeLab
+					<div className='title'> &nbsp;Co-fondatrice de l'Association LifeLab</div>
 					<p>
-						organisation d'une exposition d'art intitulée "D'autre facon de vivre" en partenariat avec la
-						Mairie du 13e arrondissement de Paris
+						Chargée de la communication et de l'organisation d'une exposition d'art intitulée "D'autre façon
+						de vivre" en partenariat avec la Mairie du 13e arrondissement de Paris
 					</p>
-					<p>Communication et organisation événementielle</p>
 				</div>
-				<div className='card three'>Traductrice independante
+				<div className='card three'>
+					<div className='title'>&nbsp;Co-fondatrice de l'Association GlobalPhoto</div>
+					<p>
+						Organisation d'une exposition photographique intitulée "Dock'ulture" en partenariat avec
+						l'Université de Bretagne Occidentale
+					</p>
 				</div>
 				<div className='card four'>
-				123
+					<img width='100%' height='40%' src={word} alt='words' />
+					<div className='title'>&nbsp; Traductrice independante</div>
+					<p>
+						Traduction de documents écrits et interprete orale aux événements pour clientele individuel et
+						entreprises chinoises <br />
+						Langues pratiquées: Chinois, Anglais, Français
+					</p>
 				</div>
 				<div className='card five'></div>
+				<div className='card six'></div>
+				<div className='card seven'>
+					<div className='title'>&nbsp;Fan de musique</div>
+
+					<p>
+						Junior's piano teacher && Rock band keyboard player <br /> Guitar && Drums beginner
+					</p>
+				</div>
+				<div className='card eight'>
+					<p>Code everyday.</p>
+				</div>
 			</div>
 		</Container>
 	);
